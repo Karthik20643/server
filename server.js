f21e76420668c176275ac8224b8ec63533037431
karthik <karthikss8590@gmail.com>
2026-09-16 23:52:52 +0530

    fixed db connection issue

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express' ;
 import cors from 'cors';
 import dotenv from 'dotenv' ; 
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ connectDB().catch((error) => {
 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
+
 
 //API routes    
 
